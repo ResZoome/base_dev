af296756192d5c7dabc503a0cbe505cbc0417c69
lancerwil <lancecode11@gmail.com>
2024-02-24 14:34:29 -0500

    testttting

---
+++ client/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button'
+
+
+export default function Header(){
+    return(
+        <>
+        <Button variant='primary'>Button</Button>
+        <span>Texted</span>
+        </>
+        )
+}
