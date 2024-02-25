1c7567dd1b242815ac6c7c035bd7a5400378d83e
lancerwil <lancecode11@gmail.com>
2024-02-24 21:08:30 -0500

    laptop

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,38 @@
 import React from "react";
-import Button from "react-bootstrap/esm/Button";
-
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
   return (
     <>
-    <div className="App">
-      <header>
-       <span>LOGO GOES HERE</span>
-      </header>
-    </div>
-    
+      <div className="App">
+        <header>
+          <span>LOGO GOES HERE</span>
+          <Container className="d-flex justify-content-center">
+            <Navbar bg="light" expand='lg' className="justify-content-between">
+              <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="me-auto">
+                    <Nav.Link href="#home" className="mr-3">Home</Nav.Link>
+                    <Nav.Link href="#features" className="mr-3">Features</Nav.Link>
+                    <Nav.Link href="#pricing" className="mr-3">Pricing</Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+
+              {/* the following should only be seen when the user is logged in */}
+              {/* <Nav>
+                <Navbar.Text>
+                  Signed in as: <a href="#login">Mark Otto</a>
+                </Navbar.Text>
+              </Nav> */}
+            </Navbar>
+          </Container>
+        </header>
+      </div>
     </>
   );
 }
